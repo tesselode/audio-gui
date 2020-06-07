51df62aa8f9f8ab406d4098bcbe84c96a3a7eab6
Andrew Minnich <aminnich3@gmail.com>
2020-06-07 15:06:37 -0400

    split drawing code into functions

--- src/backend/ggez/mod.rs
+++ src/backend/ggez/mod.rs
@@ -1,5 +1,5 @@
 use crate::gui::{
-	canvas::{ArcKind, Canvas, Color, DrawMode, DrawOperation, Style},
+	canvas::{ArcKind, Canvas, Color, DrawMode, DrawOperation, Style, TextStyle},
 	mouse_button::MouseButton,
 	point::Point,
 	rectangle::Rectangle,
@@ -108,6 +108,100 @@ impl GgezBackend {
 		self.gui.on_mouse_up(button, x, y);
 	}
 
+	fn draw_rectangle(
+		&mut self,
+		mesh_builder: &mut MeshBuilder,
+		rectangle: Rectangle,
+		style: Style,
+	) {
+		mesh_builder.rectangle(style.mode.into(), rectangle.into(), style.color.into());
+	}
+
+	fn draw_circle(
+		&mut self,
+		mesh_builder: &mut MeshBuilder,
+		position: Point,
+		radius: f32,
+		style: Style,
+	) {
+		mesh_builder.circle(style.mode.into(), position, radius, 0.1, style.color.into());
+	}
+
+	fn draw_arc(
+		&mut self,
+		mesh_builder: &mut MeshBuilder,
+		kind: ArcKind,
+		position: Point,
+		radius: f32,
+		angle1: f32,
+		angle2: f32,
+		style: Style,
+	) -> GameResult {
+		let mut points = vec![];
+		let segments = radius.ceil() as usize;
+		for i in 0..=segments {
+			let angle = angle1 + (angle2 - angle1) * (i as f32 / segments as f32);
+			points.push(Point::new(
+				position.x + radius * angle.cos(),
+				position.y + radius * angle.sin(),
+			));
+		}
+		match kind {
+			ArcKind::Pie => {
+				points.push(position);
+				points.push(Point::new(
+					position.x + radius * angle1.cos(),
+					position.y + radius * angle1.sin(),
+				));
+			}
+			ArcKind::Open => {}
+			ArcKind::Closed => {
+				points.push(Point::new(
+					position.x + radius * angle1.cos(),
+					position.y + radius * angle1.sin(),
+				));
+			}
+		}
+		match style.mode {
+			DrawMode::Fill => {
+				mesh_builder.polygon(style.mode.into(), &points, style.color.into())?;
+			}
+			DrawMode::Stroke(width) => {
+				mesh_builder.line(&points, width, style.color.into())?;
+			}
+		}
+		Ok(())
+	}
+
+	pub fn draw_text(
+		&mut self,
+		ctx: &mut Context,
+		text: String,
+		mut position: Point,
+		style: TextStyle,
+	) -> GameResult {
+		if let Some(font) = self.fonts.get(style.font_id) {
+			let mut t = Text::new(text);
+			t.set_font(
+				*font,
+				Scale {
+					x: style.size,
+					y: style.size,
+				},
+			);
+			position.x -= t.width(ctx) as f32 * style.horizontal_alignment.as_f32();
+			position.y -= t.height(ctx) as f32 * style.vertical_alignment.as_f32();
+			ggez::graphics::draw(
+				ctx,
+				&t,
+				ggez::graphics::DrawParam::new()
+					.dest(position)
+					.color(style.color.into()),
+			)?;
+		}
+		Ok(())
+	}
+
 	pub fn draw(&mut self, ctx: &mut Context) -> GameResult {
 		let mut canvas = Canvas::new();
 		self.gui.draw(&mut canvas);
@@ -119,77 +213,29 @@ impl GgezBackend {
 		for operation in canvas.operations {
 			match operation {
 				DrawOperation::Rectangle(rectangle, style) => {
-					mesh_builder.rectangle(style.mode.into(), rectangle.into(), style.color.into());
+					self.draw_rectangle(&mut mesh_builder, rectangle, style);
 					created_meshes = true;
 				}
 				DrawOperation::Circle(position, radius, style) => {
-					mesh_builder.circle(
-						style.mode.into(),
-						position,
-						radius,
-						0.1,
-						style.color.into(),
-					);
+					self.draw_circle(&mut mesh_builder, position, radius, style);
 					created_meshes = true;
 				}
 				DrawOperation::Arc(kind, position, radius, angle1, angle2, style) => {
-					let mut points = vec![];
-					let segments = radius.ceil() as usize;
-					for i in 0..=segments {
-						let angle = angle1 + (angle2 - angle1) * (i as f32 / segments as f32);
-						points.push(Point::new(
-							position.x + radius * angle.cos(),
-							position.y + radius * angle.sin(),
-						));
-					}
-					match kind {
-						ArcKind::Pie => {
-							points.push(position);
-							points.push(Point::new(
-								position.x + radius * angle1.cos(),
-								position.y + radius * angle1.sin(),
-							));
-						}
-						ArcKind::Open => {}
-						ArcKind::Closed => {
-							points.push(Point::new(
-								position.x + radius * angle1.cos(),
-								position.y + radius * angle1.sin(),
-							));
-						}
-					}
-					match style.mode {
-						DrawMode::Fill => {
-							mesh_builder.polygon(style.mode.into(), &points, style.color.into())?;
-						}
-						DrawMode::Stroke(width) => {
-							mesh_builder.line(&points, width, style.color.into())?;
-						}
-					}
+					self.draw_arc(
+						&mut mesh_builder,
+						kind,
+						position,
+						radius,
+						angle1,
+						angle2,
+						style,
+					)?;
 					created_meshes = true;
 				}
 				DrawOperation::Polyline(_, _) => todo!(),
 				DrawOperation::Polygon(_, _) => todo!(),
-				DrawOperation::Text(text, mut position, style) => {
-					if let Some(font) = self.fonts.get(style.font_id) {
-						let mut t = Text::new(text);
-						t.set_font(
-							*font,
-							Scale {
-								x: style.size,
-								y: style.size,
-							},
-						);
-						position.x -= t.width(ctx) as f32 * style.horizontal_alignment.as_f32();
-						position.y -= t.height(ctx) as f32 * style.vertical_alignment.as_f32();
-						ggez::graphics::draw(
-							ctx,
-							&t,
-							ggez::graphics::DrawParam::new()
-								.dest(position)
-								.color(style.color.into()),
-						)?;
-					}
+				DrawOperation::Text(text, position, style) => {
+					self.draw_text(ctx, text, position, style)?;
 				}
 			}
 		}
