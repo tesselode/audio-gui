fd08468a19a069bcf3f5203ccb15337936dad1e0
Andrew Minnich <aminnich3@gmail.com>
2020-06-27 15:17:43 -0400

    add API niceties to the Rect struct (untested)

--- knobs/src/geometry/rect.rs
+++ knobs/src/geometry/rect.rs
@@ -7,18 +7,30 @@ pub struct Rect {
 }
 
 impl Rect {
-	pub fn new(x: f32, y: f32, width: f32, height: f32) -> Self {
+	pub fn new() -> Self {
+		Self {
+			position: Vector::zero(),
+			size: Vector::zero(),
+		}
+	}
+
+	pub fn from_xywh(x: f32, y: f32, width: f32, height: f32) -> Self {
 		Self {
 			position: Vector::new(x, y),
 			size: Vector::new(width, height),
 		}
 	}
 
-	pub fn shifted(self, translation: Vector) -> Self {
-		Self {
-			position: self.position + translation,
-			..self
-		}
+	pub fn x(&self, origin: f32) -> f32 {
+		self.position.x + self.size.x * origin
+	}
+
+	pub fn y(&self, origin: f32) -> f32 {
+		self.position.y + self.size.y * origin
+	}
+
+	pub fn position(&self, origin: Vector) -> Vector {
+		Vector::new(self.x(origin.x), self.y(origin.y))
 	}
 
 	pub fn contains_point(&self, point: Vector) -> bool {
@@ -27,10 +39,74 @@ impl Rect {
 			&& point.y >= self.position.y
 			&& point.y <= self.position.y + self.size.y
 	}
+
+	pub fn set_width(&mut self, width: f32) {
+		self.size.x = width;
+	}
+
+	pub fn with_width(mut self, width: f32) -> Self {
+		self.set_width(width);
+		self
+	}
+
+	pub fn set_height(&mut self, height: f32) {
+		self.size.y = height;
+	}
+
+	pub fn with_height(mut self, height: f32) -> Self {
+		self.set_height(height);
+		self
+	}
+
+	pub fn set_size(&mut self, size: Vector) {
+		self.size = size;
+	}
+
+	pub fn with_size(mut self, size: Vector) -> Self {
+		self.set_size(size);
+		self
+	}
+
+	pub fn set_x(&mut self, x: f32, origin: f32) {
+		self.position.x = x - self.size.x * origin;
+	}
+
+	pub fn at_x(mut self, x: f32, origin: f32) -> Self {
+		self.set_x(x, origin);
+		self
+	}
+
+	pub fn set_y(&mut self, y: f32, origin: f32) {
+		self.position.y = y - self.size.y * origin;
+	}
+
+	pub fn at_y(mut self, y: f32, origin: f32) -> Self {
+		self.set_y(y, origin);
+		self
+	}
+
+	pub fn set_position(&mut self, position: Vector, origin: Vector) {
+		self.position.x = position.x - self.size.x * origin.x;
+		self.position.y = position.y - self.size.y * origin.y;
+	}
+
+	pub fn with_position(mut self, position: Vector, origin: Vector) -> Self {
+		self.set_position(position, origin);
+		self
+	}
+
+	pub fn shift(&mut self, translation: Vector) {
+		self.position += translation;
+	}
+
+	pub fn shifted(mut self, translation: Vector) -> Self {
+		self.shift(translation);
+		self
+	}
 }
 
 impl Default for Rect {
 	fn default() -> Self {
-		Self::new(0.0, 0.0, 0.0, 0.0)
+		Self::from_xywh(0.0, 0.0, 0.0, 0.0)
 	}
 }
