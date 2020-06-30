996705977e2d20f9773912010482ceace20fb162
Andrew Minnich <aminnich3@gmail.com>
2020-06-30 05:11:51 -0400

    remove test events from the Rectangle component

--- knobs/src/component/rectangle.rs
+++ knobs/src/component/rectangle.rs
@@ -35,20 +35,6 @@ impl Rectangle {
 }
 
 impl Component for Rectangle {
-	fn on(&mut self, event: &Event, _elements: &mut Elements, _event_queue: &mut EventQueue) {
-		match event {
-			Event::Hover(id, position) => println!("Event::Hover({:?}, {:?})", id, position),
-			Event::Unhover(id) => println!("Event::Unhover({:?})", id),
-			Event::Press(id, button) => println!("Event::Press({:?}, {:?})", id, button),
-			Event::Release(id, button) => println!("Event::Release({:?}, {:?})", id, button),
-			Event::Click(id, button) => println!("Event::Click({:?}, {:?})", id, button),
-			Event::Drag(id, button, position, delta) => println!(
-				"Event::Drag({:?}, {:?}, {}, {})",
-				id, button, position, delta
-			),
-		}
-	}
-
 	fn draw_below(&self, element: &Element, canvas: &mut Canvas, _resources: &Resources) {
 		if let Some(color) = self.fill {
 			canvas.draw_rectangle(element.rect, ShapeStyle::Fill(color));
